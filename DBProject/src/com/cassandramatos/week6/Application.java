fc1f96f62e8728982f0933a63f3f51255a4a9031
Sri Srinivasan <sri.dhcs@gmail.com>
2020-12-09 20:20:04 -0800

    Added a Sri line.

--- DBProject/src/com/cassandramatos/week6/Application.java
+++ DBProject/src/com/cassandramatos/week6/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("It's connected.");
+		System.out.println("Sri entered this.");
 	}
 
 }
